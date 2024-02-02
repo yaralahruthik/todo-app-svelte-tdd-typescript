a7766a59594bc3ac6cb7fd31696a9d44b66a3767
yaralahruthik <yaralahruthikreddy@gmail.com>
2024-02-02 05:55:11 +0530

    reading tasks

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,24 @@
+<script lang="ts">
+  type Priority = 'p1' | 'p2' | 'p3';
+
+  type Task = {
+    id: number;
+    title: string;
+    isCompleted: boolean;
+    priority?: Priority;
+  };
+
+  const tasks: Task[] = [
+    { id: 1, title: 'Learn Svelte', isCompleted: false },
+    { id: 2, title: 'Learn TypeScript', isCompleted: false },
+    { id: 3, title: 'Learn TDD', isCompleted: false },
+  ];
+</script>
+
 <h1>Tasks</h1>
 
 <ul>
-  <li>Learn Svelte</li>
-  <li>Learn TypeScript</li>
-  <li>Learn TDD</li>
+  {#each tasks as task (task.id)}
+    <li>{task.title}</li>
+  {/each}
 </ul>
